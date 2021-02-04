60736e4d03a6970a7c420870300d6147ad8c96eb
Thomas Zanis <56691946+campingtomz@users.noreply.github.com>
2021-02-04 15:48:04 -0500

    update

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[Controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -23,6 +22,7 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
         //api/Users/id
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id){
             return await _context.Users.FindAsync(id);
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model:any ={};
+  constructor(public AccountService: AccountService, 
+    private router: Router, 
+    private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+  login(){
+    this.AccountService.login(this.model).subscribe(response =>{
+      this.router.navigateByUrl('/members');
+    });
+  }
+  logout(){
+    this.router.navigateByUrl('/');
+
+    this.AccountService.logout();
+    
+  }
+  
+}
